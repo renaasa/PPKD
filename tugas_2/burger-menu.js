8b4d68f202cf3cf61a8d9bc7dc712e0fa6e547fc
Renata <renataa.annisa@gmail.com>
2025-07-31 15:00:52 +0700

    Add files via upload

---
+++ tugas_2/burger-menu.js
@@ -0,0 +1,5 @@
+// kalo udah dibuat folder js sendiri, tidak perlu pakai function
+function togleMenu() {
+  document.getElementById("menu").classList.toggle("show");
+}
+// selain getElementById, bisa gunakan querySelector dengan menggunakan simbol #menu
